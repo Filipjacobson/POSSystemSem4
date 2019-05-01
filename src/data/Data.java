89b1618e77e656cc6b4d8bc559440aef7a6b25b6
Filip Jacobson <filip.h.jacobson@gmail.com>
2019-05-01 22:12:34 +0200

    Fifth commit

--- src/data/Data.java
+++ src/data/Data.java
@@ -4,19 +4,36 @@ import util.CustomerIdentification;
 import util.ItemIdentifier;
 import integration.ItemDTO;
 
+/**
+ *
+ */
 public class Data {
 
+    /**
+     *
+     */
     public Data(){
 
     }
 
-    //Method for finding the itemInformationData based on itemIdentifier is not part of this task.
+    /**
+     * Method for finding the itemInformationData based on itemIdentifier is not part of this task.
+     *
+     * @param itemIdentifier
+     * @return
+     */
+    //
     public ItemDTO getItemInformationData(ItemIdentifier itemIdentifier){
 
         return null;
     }
 
-    //Method for finding the customerDiscountData based on customerIdentification is not part of this task.
+    /**
+     * Method for finding the customerDiscountData based on customerIdentification is not part of this task.
+     *
+     * @param customerIdentification
+     * @return
+     */
     public double getCustomerDiscountData(CustomerIdentification customerIdentification){
         return null;
     }
