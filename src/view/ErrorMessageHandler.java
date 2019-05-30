7add8b54bc9aca37a2080cf9c9526c62d171e109
Filip Jacobson <filip.h.jacobson@gmail.com>
2019-05-30 11:01:49 +0200

    Adding ErrorMessageHandler

--- src/view/ErrorMessageHandler.java
+++ src/view/ErrorMessageHandler.java
@@ -1,4 +1,34 @@
 package view;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+
+/**
+ * Responsible for showing the user the error messages.
+ */
 public class ErrorMessageHandler {
+
+    /**
+     *
+     * @param msg
+     */
+    void showErrorMessage(String msg){
+        StringBuilder errorMsgBuild = new StringBuilder();
+        errorMsgBuild.append(createTime());
+        errorMsgBuild.append(", ERROR MSG:");
+        errorMsgBuild.append(msg);
+        System.out.println(errorMsgBuild);
+    }
+
+    /**
+     *
+     * @return
+     */
+    private String createTime(){
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        return time.format(dtf);
+    }
 }
