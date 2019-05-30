ea613b9c53bc48d974f80e364dbd43dd24b1f9fa
Filip Jacobson <filip.h.jacobson@gmail.com>
2019-05-30 15:48:31 +0200

    Adding test for ErrorMessageHandler

---
+++ test/view/ErrorMessageHandlerTest.java
@@ -0,0 +1,36 @@
+package view;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ErrorMessageHandlerTest {
+    /**
+     *
+     */
+    @Before
+    public void setUp(){
+
+    }
+
+    /**
+     *
+     */
+    @After
+    public void tearDown(){
+
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testOperationFailureException() throws Exception {
+
+    }
+}
+
+
